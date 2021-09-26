47a33428ee960cee7789e596a4c2ad39360d25d1
sandeep <sandeep06011991@gmail.com>
2021-09-26 06:02:19 -0400

    added dnn layers, tests not cleaned

--- tests/linear_test.cc
+++ tests/linear_test.cc
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <string>
-#include "layers/loss.hh"
+#include "layers/linear.hh"
+#include "layers/relu.hh"
 #include "tensor.hh"
 #include <fstream>
 
 std::string BIN_DIR = "/mnt/homes/spolisetty/data/tests/linear";
 
 int main(){
-  N = 1024
-  M = 40
-  K = 16
+  int N = 1024;
+  int M = 40;
+  int K = 16;
+  // N = 4;
+  // M = 4;
+  // K = 12;
 
   std::fstream file1(BIN_DIR + "/input.bin",std::ios::in|std::ios::binary);
   float * input = (float *)malloc (N * M  * sizeof(float));
@@ -17,42 +21,52 @@ int main(){
   Tensor<float> * input_t = new Tensor<float>(input, N, M);
 
   std::fstream file2(BIN_DIR + "/W.bin",std::ios::in|std::ios::binary);
-  float * w  = (float *)malloc (sizeof(float) * M * K);
-  file2.read((char *)w, sizeof(float) * M * K);
-  Tensor<float> * W_t = new Tensor<float>(w, M, K);
+  float * W  = (float *)malloc (sizeof(float) * M * K);
+  file2.read((char *)W, sizeof(float) * M * K);
 
-  std::fstream file2(BIN_DIR + "/b.bin",std::ios::in|std::ios::binary);
-  float * B  = (float *)malloc (sizeof(float) * M);
-  file2.read((char *)B, sizeof(float) * M * 1);
-  Tensor<float> * B_t = new Tensor<float>(B, M, 1);
+  std::fstream file3(BIN_DIR + "/b.bin",std::ios::in|std::ios::binary);
+  float * B  = (float *)malloc (sizeof(float) * K);
+  file3.read((char *)B, sizeof(float) * K * 1);
 
-  std::fstream file1(BIN_DIR + "/out.bin",std::ios::in|std::ios::binary);
+  std::fstream file4(BIN_DIR + "/out2.bin",std::ios::in|std::ios::binary);
   float * output = (float *)malloc (N * K  * sizeof(float));
-  file1.read((char *)output , N * K  * sizeof(float));
+  file4.read((char *)output , N * K  * sizeof(float));
   Tensor<float> * output_t = new Tensor<float>(output, N, K);
 
-  std::fstream file2(BIN_DIR + "/dW.bin",std::ios::in|std::ios::binary);
+  std::fstream file5(BIN_DIR + "/dW.bin",std::ios::in|std::ios::binary);
   float * dw  = (float *)malloc (sizeof(float) * M * K);
-  file2.read((char *)dw, sizeof(float) * M * K);
+  file5.read((char *)dw, sizeof(float) * M * K);
   Tensor<float> * dw_t = new Tensor<float>(dw, M, K);
 
-  std::fstream file2(BIN_DIR + "/db.bin",std::ios::in|std::ios::binary);
-  float * db  = (float *)malloc (sizeof(float) * M);
-  file2.read((char *)db, sizeof(float) * M * 1);
-  Tensor<float> * db_t = new Tensor<float>(db, M, 1);
+  std::fstream file6(BIN_DIR + "/db.bin",std::ios::in|std::ios::binary);
+  float * db  = (float *)malloc (sizeof(float) * K);
+  file6.read((char *)db, sizeof(float) * K * 1);
+  Tensor<float> * db_t = new Tensor<float>(db, K, 1);
 
-  LinearLayer* l1 = new LinearLayer(W_l,B_t, N, M, K);
+  std::fstream file7(BIN_DIR + "/dX.bin",std::ios::in|std::ios::binary);
+  float * dX = (float *)malloc(sizeof(float) * N * M );
+  file7.read((char *)dX , N * M  * sizeof(float));
+  Tensor<float> * dX_t = new Tensor<float>(dX, N, M);
+
+
+  LinearLayer* l1 = new LinearLayer(W,B, M, K,N);
   Relu* l2 = new Relu(N,K);
+  // // Construct tensors
+  // // Match forward pass
+  // // check output at l2
+  auto i = l1->computeForwardPass(input_t);
+  auto out = l2->forward(i);
+  auto g = l2->backward(*allocate_ones(N,K));
+  auto dX_calc = l1->computeBackwardPass(g);
 
-  // Construct tensors
-  // Match forward pass
-  // check output at l2
-
-  // Tensor<float> *ones = getOnes(N,K);
-  // auto g = l2->backward(ones);
-  // l1->backward();
-  // Match backward pass
-  // Check grad_w, grad_b
-
-  std::cout << "Test ok !\n";
+  dX_t->debugTensor();
+  dX_calc.debugTensor();
+  dw_t->debugTensor();
+  l1->dW->debugTensor();
+  db_t->debugTensor();
+  l1->db->debugTensor();
+  // out.debugTensor();
+  // std::cout << "Final Target \n";
+  // output_t->debugTensor();
+  // std::cout << "Test ok !\n";
 }
--- tests/linear_test.py
+++ tests/linear_test.py
@@ -5,9 +5,14 @@ import torch
 N = 1024
 M = 40
 K = 16
+# N = 4
+# M = 4
+# K = 12
 
 input = torch.rand((N,M),requires_grad = True)
 layer1 = torch.nn.Linear(M, K)
+print(layer1.weight.shape)
+# assert(False)
 layer2 = torch.nn.ReLU()
 
 out1 = layer1(input)
@@ -20,18 +25,24 @@ output.sum().backward()
 with open(TARGET_DIR+'/input.bin','wb') as fp:
     fp.write(input.detach().numpy().astype('float32').tobytes())
 with open(TARGET_DIR+'/W.bin','wb') as fp:
-    fp.write(layer1.weight.detach().numpy().astype('float32').tobytes())
-with open(TARGET_DIR+'/b.bin','wb') as fp:
-    fp.write(layer1.bias.detach().numpy().astype('float32').tobytes())
+    fp.write(layer1.weight.T.detach().numpy().astype('float32').tobytes())
+if layer1.bias!=None:
+    assert(layer1.bias.shape == (K,))
+    with open(TARGET_DIR+'/b.bin','wb') as fp:
+        fp.write(layer1.bias.detach().numpy().astype('float32').tobytes())
 ### Write intermediate outputs
 with open(TARGET_DIR+'/out1.bin','wb') as fp:
-    fp.write(out1.detach().numpy().astype('int').tobytes())
+    fp.write(out1.detach().numpy().astype('float32').tobytes())
 with open(TARGET_DIR+'/out2.bin','wb') as fp:
-    fp.write(output.detach().numpy().astype('int').tobytes())
+    fp.write(output.detach().numpy().astype('float32').tobytes())
 ### Write intermediate gradients
 with open(TARGET_DIR+'/dW.bin','wb') as fp:
-    fp.write(layer1.weight.grad.numpy().astype('int').tobytes())
-with open(TARGET_DIR+'/db.bin','wb') as fp:
-    fp.write(layer1.bias.grad.numpy().astype('int').tobytes())
+    fp.write(layer1.weight.grad.T.numpy().astype('float32').tobytes())
+with open(TARGET_DIR+'/dX.bin','wb') as fp:
+    fp.write(input.grad.numpy().astype('float32').tobytes())
+print(input.grad)
+if layer1.bias!=None:
+    with open(TARGET_DIR+'/db.bin','wb') as fp:
+        fp.write(layer1.bias.grad.numpy().astype('float32').tobytes())
 
 print("All ok !!")
