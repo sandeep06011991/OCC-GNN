b83fcfe6358798020c9a7d6dc53bec82f8a29575
MiniShark27 <kobifalus@gmail.com>
2022-07-21 01:39:22 -0400

    update DGL

---
+++ cslicer/build.sh
@@ -0,0 +1,2 @@
+# Because I keep forgetting the command to build this
+python setup.py build_ext --inplace
---
+++ init.sh
@@ -0,0 +1,8 @@
+# Run as . ./init.sh from the OCC-GNN directory
+. venv/bin/activate
+export PYTHONPATH=`pwd`/cslicer
+cd python
+
+# For Reference:
+# Build Cslicer (run from cslicer directory)
+# python setup.py build_ext --inplace
