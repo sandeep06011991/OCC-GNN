97009ef00cdab2577ba7b9c0cf95528c5ad8c06a
MiniShark27 <kobifalus@gmail.com>
2022-07-21 16:32:10 -0400

    fix logmaker

--- python/logmaker.sh
+++ python/logmaker.sh
@@ -1,8 +1,3 @@
-# mkdir -p logs
-# printf -v VAR "logs/%(%Y-%m-%d %H-%M-%S)T $1.log" -1
-# python3 $1 > "$VAR"
-
-# mkdir -p logs
 printf -v DIRECTORY "logs/%(%Y-%m-%d)T" -1
 printf -v FILE "$DIRECTORY/%(%H-%M-%S)T $1.log" -1
 mkdir -p $DIRECTORY
