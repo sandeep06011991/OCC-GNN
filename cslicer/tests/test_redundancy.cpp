77c5075dbe1b3edade5928105b9dde0d4bd8b8ad
sandeep06011991 <sandeep06011991@gmail.com>
2022-11-20 20:06:02 -0500

    redundancy

--- cslicer/tests/test_redundancy.cpp
+++ cslicer/tests/test_redundancy.cpp
@@ -25,7 +25,10 @@ redundant get_red_stats(PartitionedSample &ps){
 
 }
 
-void test_reduction_communication_computation(Sample &s,std::vector<int> workload_map,
+// This test is incorrect.
+// Baselines sample from redudnat computation.
+// Leading to graph with different fanout
+void test_reduction_communication_computation(vector<long> minibatch,std::vector<int> workload_map,
           std::vector<int> storage[4], std::vector<int> storage_map[4], int rounds ){
       vector<int> ** layer_color = (vector<int> **)malloc(4 * sizeof(vector<int> *));
       for(int i=0;i<4;i++){
@@ -35,17 +38,19 @@ void test_reduction_communication_computation(Sample &s,std::vector<int> workloa
               layer_color[i]->push_back(0);
         }
       }
+
+      redundant r1 = print_statistics(s, layer_color,workload_map.size(), workload_map, storage_map);
+
       bool self_edge = false;
       bool pull_optim= true;
       Slice *s1 =   new Slice(workload_map, storage, self_edge, rounds, pull_optim);
-      std::cout << s.num_layers <<" ";
-      PartitionedSample ps1(s.num_layers);
-      s1->slice_sample(s, ps1);
-
-      redundant r1 = print_statistics(s, layer_color,workload_map.size(), workload_map, storage_map);
-      redundant r2 = get_red_stats(ps1) ;
-      std::cout << "Baseline\n";
-      r1.debug();
-      std::cout << "My approach\n";
-      r2.debug();
+      // std::cout << s.num_layers <<" ";
+      // PartitionedSample ps1(s.num_layers);
+      // s1->slice_sample(s, ps1);
+      //
+      // redundant r2 = get_red_stats(ps1) ;
+      // std::cout << "Baseline\n";
+      // r1.debug();
+      // std::cout << "My approach\n";
+      // r2.debug();
 }
